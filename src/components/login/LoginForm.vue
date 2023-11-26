e22e024e173985fcf440223388cbd1171f0ac980
Ho Le Quoc Tin <holequoctin@gmail.com>
2023-11-26 16:04:30 +0700

    initial commit

---
+++ src/components/login/LoginForm.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>This is login form</h1>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>This is login view</h1>
+</template>
+
+<script setup>
+
+</script>
