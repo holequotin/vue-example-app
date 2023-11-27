9f83e7bb5992d7f5ab7de526d1b52e70135a0017
Ho Le Quoc Tin <holequoctin@gmail.com>
2023-11-27 15:20:01 +0700

    change register dialog close button

--- src/components/login/RegisterForm.vue
+++ src/components/login/RegisterForm.vue
@@ -6,11 +6,13 @@
             </v-btn>
         </template>
         <v-card>
-            <v-card-title>
+            <v-card-title class="d-flex justify-space-between">
                 <span class="text-h5">Create an account</span>
+                <span> <v-btn icon dark @click="dialog = false">
+                        <v-icon>mdi-close</v-icon>
+                    </v-btn></span>
             </v-card-title>
             <v-card-subtitle>
-
                 Fast and easy
             </v-card-subtitle>
             <v-card-text>
@@ -37,9 +39,6 @@
                         </v-col>
                         <v-col cols="12" sm="12">
                             <v-btn round color="primary" dark block type="submit">Register</v-btn>
-                            <v-btn icon dark @click="dialog = false">
-                                <v-icon>mdi-close</v-icon>
-                            </v-btn>
                         </v-col>
                     </v-row>
                 </v-container>
@@ -47,9 +46,6 @@
             </v-card-text>
             <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
-                    Close
-                </v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
