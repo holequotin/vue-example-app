ae2c3a7facfb1d33db0ae5747aad2e01058c646a
Ho Le Quoc Tin <holequoctin@gmail.com>
2024-04-24 09:08:43 +0700

    pagination for friend request

--- src/views/FriendRequest.vue
+++ src/views/FriendRequest.vue
@@ -8,6 +8,14 @@
                 <h1 v-if="friendships.length == 0">There are no friend requests</h1>
                 <FriendRequest v-else v-for="friendship in friendships" v-bind:key="friendship.id" :friendship="friendship"
                     @accept="() => getFriendRequests(currentPage)" @refuse="() => getFriendRequests(currentPage)"></FriendRequest>
+              <div class="text-center">
+                <v-pagination
+                  v-model="currentPage"
+                  :length="meta.last_page"
+                  :total-visible="7"
+                  @update:model-value="() => getFriendRequests(currentPage)"
+                ></v-pagination>
+              </div>
             </v-main>
         </template>
         <template #left-drawer>
@@ -19,22 +27,24 @@
     </BaseLayout>
 </template>
 <script setup>
-import BaseLayout from './base/BaseLayout.vue';
+import BaseLayout from './base/BaseLayout.vue'
 import AppBar from '../components/home/appbar/AppBar.vue'
-import DrawerLeft from '../components/home/drawerLeft/DrawerLeft.vue';
-import DrawerRight from '../components/home/drawerRight/DrawerRight.vue';
-import { useUserStore } from '@/stores/user';
-import { ref, watchEffect } from 'vue';
+import DrawerLeft from '../components/home/drawerLeft/DrawerLeft.vue'
+import DrawerRight from '../components/home/drawerRight/DrawerRight.vue'
+import { useUserStore } from '@/stores/user'
+import { ref, watchEffect } from 'vue'
 import { friendService } from '@/service/friendService'
-import { errorHandler } from '@/utils/errorHandler';
-import FriendRequest from '../components/friends/FriendRequest.vue';
+import { errorHandler } from '@/utils/errorHandler'
+import FriendRequest from '../components/friends/FriendRequest.vue'
 
 
 const userStore = useUserStore()
 const user = ref(userStore.user);
 const friendships = ref([])
 const currentPage = ref(1)
-
+const meta = ref({
+  last_page: 1
+})
 userStore.getUser()
 
 user.value = userStore.user
@@ -48,6 +58,7 @@ function getFriendRequests(page) {
         .then((response) => {
             console.log(response.data)
             friendships.value = response.data.data
+          meta.value = response.data.meta
         })
         .catch((error) => {
             errorHandler(error)
